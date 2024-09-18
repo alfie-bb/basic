b87be8e265846f9031ec9cf403c8bbe6a1429412
alfie-bb <98765852+alfie-bb@users.noreply.github.com>
2024-09-18 09:28:39 +0100

    added login

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,13 @@ st.title("🎈 Alfie's Shack")
 st.write("Enter your name when prompted")
 
 name = st.text_input("Please enter yor name ")
-st.write("Hi",name)
+
+
+def register():
+    surname = st.text_input("Enter your surname: ")
+    forename = st.text_input("Enter your forname: ")
+    st.write("Hi"+forename+"!")
+    username = st.text_input("Choose a username: ")
+    password = st.text_input("Password: ")
+    f = open("users.txt", "a")
+    f.write("Username:",username,"\nForename:",forename,"\nSurname:",surname,"\nPassword:",password,"\n\n")
