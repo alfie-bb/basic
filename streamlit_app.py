7ebddc7ac94ad07630933f313f65f983cea6ac9c
alfie-bb <98765852+alfie-bb@users.noreply.github.com>
2024-09-18 10:08:08 +0100

    panda

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pandas as pd
 st.title("🎈 Alfie's Shack")
 st.write("Enter your name when prompted")
 
@@ -13,7 +13,7 @@ def register():
     username = st.text_input("Choose a username: ")
     password = st.text_input("Password: ")
     st.button("Submit")
-    f = open("users.txt", "a")
+    f = open("users.csv", "a", "utf-8-sig")
     f.write("Username: "+username+"\nForename: "+forename+"\nSurname: "+surname+"\nPassword: "+password+"\n\n")
     f.close()
 
