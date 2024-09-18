9303cd892cef747dfd3f63d25a2e16dcb98fc9fb
alfie-bb <98765852+alfie-bb@users.noreply.github.com>
2024-09-18 10:12:28 +0100

    dir changed

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ def register():
     username = st.text_input("Choose a username: ")
     password = st.text_input("Password: ")
     st.button("Submit")
-    f = open("users.csv", "a", "utf-8-sig")
+    f = open("users.csv", "a")
     f.write("Username: "+username+"\nForename: "+forename+"\nSurname: "+surname+"\nPassword: "+password+"\n\n")
     f.close()
 
