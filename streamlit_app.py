11c7b74b550b187317c969ad73a99c89f31ebef3
alfie-bb <98765852+alfie-bb@users.noreply.github.com>
2024-09-18 09:40:17 +0100

    dir change

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,6 @@ def register():
     st.write("Hi"+forename+"!")
     username = st.text_input("Choose a username: ")
     password = st.text_input("Password: ")
-    f = open("users.txt", "a")
+    f = open("alfie-bb/basic/users.txt", "a")
     f.write("Username:",username,"\nForename:",forename,"\nSurname:",surname,"\nPassword:",password,"\n\n")
     f.close()
